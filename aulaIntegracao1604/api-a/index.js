5c5cf3a7a3077b70d064313f0ab06985befac16b
Jorge Willian Paez Nagakura <jorge.nagakura@sistemafiep.org.br>
2025-04-16 21:29:26 -0300

    Adicionando cache

--- aulaIntegracao1604/api-a/index.js
+++ aulaIntegracao1604/api-a/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const redis = require('redis');
 const port = 3000;
 
 const WEATHER_API_URL = 'http://localhost:3001/weather';
+
+const redisClient = redis.createClient();
+
+redisClient.on('error', (err) => {
+    console.error('Erro no Redis:', err);
+});
+
+(async () => {
+    await redisClient.connect();
+})();
 
 app.get('/recommendation/:city', async (req, res) => {
   const city = req.params.city;
 
   try {
+
+    const cachedData = await redisClient.get(city);
+
+    if (cachedData) {
+      console.log(`Carregando cidade usando o cache`);
+      return res.json(JSON.parse(cachedData));
+    }
+
     const response = await axios.get(`${WEATHER_API_URL}/${city}`);
-    const { temp, unit } = response.data;
+    const { city: cityName, temp, unit } = response.data;
 
     let recommendation = '';
     if (temp > 30) {
@@ -21,13 +40,11 @@ app.get('/recommendation/:city', async (req, res) => {
       recommendation = 'Está frio! Não esqueça de usar um casaco.';
     }
 
-    res.json({
-      city: response.data.city,
-      temp,
-      unit,
-      recommendation
-    });
+    const result = { city: cityName, temp, unit, recommendation };
 
+    await redisClient.setEx(city, 60, JSON.stringify(result));
+
+    res.json(result);
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar dados do clima' });
   }
